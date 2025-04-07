fff7744d15903c896552ab770d5862d42efe0aa0
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-04-07 22:34:07 +0100

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
  
 const inter = Inter({ subsets: ['latin'], weight: '400' });
@@ -16,6 +18,8 @@ export default function RootLayout({
       </head>
       <body>
           {children}
+          <SpeedInsights />
+          <Analytics />
       </body>
     </html>
   );
